dd5c9e5d1d152025199ae74dcfd5c350eb358305
ngadiono <ngadiono@djavaui.com>
2019-07-01 11:09:46 +0700

    update example page

--- packages/views/example/Container/index.js
+++ packages/views/example/Container/index.js
@@ -7,22 +7,21 @@ import './style.scss';
 class Container extends Component {    
     render() {
         return (
-            <div className="App">
-                <Header />
-                <header className="App-header">
-                    <img src="https://www.qasir.id/images/revamp-new-img/Qasir-White.svg" className="App-logo" alt="logo" />
+            <div className="app">                
+                <div className="app-header">
+                    <img src="https://www.qasir.id/images/revamp-new-img/Qasir-White.svg" className="app-logo" alt="logo" />
                     <p>
                     Qasir's Frontend Mitra (ReactJS Seed) has been designed to provide you with a super fast test driven front-end development lifecycle.
                     </p>
                     <a
-                    className="App-link"
+                    className="app-link"
                     href="https://qasir-id.atlassian.net/wiki/spaces/WTH/pages/28835892/QM+-+Frontend+Documentation"
                     target="_blank"
                     rel="noopener noreferrer"
                     >
                     Learn More
                     </a>
-                </header>
+                </div>
             </div>
         )
     }
