6ec7058ab03c0264f8d813bf7d4f5fe1ac2cccba
ngadiono <ngadiono@djavaui.com>
2020-05-08 14:23:01 +0700

    adding antd in dashboard

--- dashboard/pages/_app.js
+++ dashboard/pages/_app.js
@@ -2,6 +2,10 @@ import App from 'next/app';
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
+import 'antd/dist/antd.css';
+import '../styles/vars.css';
+import '../styles/global.css';
+
 const theme = {
   colors: {
     primary: '#f04b32',
