cb842ad8ca2a23d5a9cc8e1486905e86b9123847
ngadiono <ngadiono@djavaui.com>
2020-05-13 17:00:19 +0700

    adding title tab config

--- js/dashboard/pages/_app.js
+++ js/dashboard/pages/_app.js
@@ -1,12 +1,13 @@
 // Vendor packages
 import React from 'react';
 import App from 'next/app';
-import { ThemeProvider, createGlobalStyle, css } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 
 // Styles
 import 'antd/dist/antd.css';
 import StyleReset from '../styles/reset';
 
+const appName = 'Qasir Boilerplate';
 const theme = {
   colors: {
     primary: '#f04b32',
@@ -23,7 +24,7 @@ export default class MyApp extends App {
       <ThemeProvider theme={theme}>
         <StyleReset />
         <Layout>
-          <Component {...pageProps} />
+          <Component {...pageProps} appName={appName} />
         </Layout>
       </ThemeProvider>
     );
--- js/dashboard/pages/auth/login/index.js
+++ js/dashboard/pages/auth/login/index.js
@@ -1,50 +1,66 @@
-// Vendor packages
-import React from 'react';
+// Next
+import Head from 'next/head';
+import Link from 'next/link';
+
+// Vendors
 import { Form, Input, Button, Checkbox } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
-import Link from 'next/link';
 
 // Styles
 import { Wrapper } from './style';
 
-const Login = () => {
+const Login = ({ appName }) => {
   const onFinish = (values) => {
     console.log('Received values of form: ', values);
   };
 
   return (
-    <Wrapper>
-      <Form name="normal_login" className="login-form" initialValues={{ remember: true }} onFinish={onFinish}>
-        <Form.Item name="username" rules={[{ required: true, message: 'Please input your Username!' }]}>
-          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-        </Form.Item>
-        <Form.Item name="password" rules={[{ required: true, message: 'Please input your Password!' }]}>
-          <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
-        <Form.Item>
-          <Form.Item name="remember" valuePropName="checked" noStyle>
-            <Checkbox>Remember me</Checkbox>
+    <>
+      <Head>
+        <title>Member Area - {appName}</title>
+      </Head>
+      <Wrapper>
+        <img src="https://dummyimage.com/400x200/000/fff" alt="logo" style={{ marginBottom: '50px' }} />
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+        >
+          <Form.Item name="username" rules={[{ required: true, message: 'Please input your Username!' }]}>
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+          </Form.Item>
+          <Form.Item name="password" rules={[{ required: true, message: 'Please input your Password!' }]}>
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Link href="/auth/forgot">
+              <a className="login-form-forgot">Forgot password</a>
+            </Link>
           </Form.Item>
 
-          <Link href="/auth/forgot">
-            <a className="login-form-forgot">Forgot password</a>
-          </Link>
-        </Form.Item>
-
-        <Form.Item>
-          <Link href="/dashboard">
-            <Button type="primary" className="login-form-button">
-              Log in
-            </Button>
-          </Link>
-          Or <Link href="/auth/register">register now!</Link>
-        </Form.Item>
-      </Form>
-    </Wrapper>
+          <Form.Item>
+            <Link href="/dashboard">
+              <Button type="primary" className="login-form-button" style={{ marginRight: '10px' }}>
+                Log in
+              </Button>
+            </Link>
+            Or{' '}
+            <Link href="/auth/register">
+              <a>register now!</a>
+            </Link>
+          </Form.Item>
+        </Form>
+      </Wrapper>
+    </>
   );
 };
 
