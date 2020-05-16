6c43ead7828538d0903cd8cc331bf1aec75377d6
ngadiono <ngadiono@djavaui.com>
2020-05-16 17:12:20 +0700

    adding sst caching

--- js/dashboard/server.js
+++ js/dashboard/server.js
@@ -1,3 +1,4 @@
+const cacheableResponse = require('cacheable-response');
 const express = require('express');
 const next = require('next');
 const bodyParser = require('body-parser');
@@ -7,11 +8,31 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const ssrCache = cacheableResponse({
+  ttl: 1000 * 60 * 60, // 1hour
+  get: async ({ req, res, pagePath, queryParams }) => {
+    const data = await app.renderToHTML(req, res, pagePath, queryParams);
+
+    // Add here custom logic for when you do not want to cache the page, for
+    // example when the page returns a 404 status code:
+    if (res.statusCode === 404) {
+      res.end(data);
+      return;
+    }
+
+    return { data };
+  },
+  send: ({ data, res }) => res.send(data),
+});
+
 app.prepare().then(() => {
   const server = express();
 
   server.use(bodyParser.urlencoded({ extended: true }));
   server.use(bodyParser.json());
+
+  // Here example for using SSR Caching
+  // server.get('/', (req, res) => ssrCache({ req, res, pagePath: '/' }))
 
   server.all('*', (req, res) => {
     return handle(req, res);
